582204ba4b72c338f8e48bc6bfbff1980143848b
mltesty <mltesty1234@googlemail.com>
2016-05-12 12:32:29 +0200

    Update checkDivBg.js

--- tests/checkDivBg.js
+++ tests/checkDivBg.js
@@ -2,6 +2,6 @@ describe('getDivBg', function() {
     var d = document.querySelector('.box');
 
     it('Should be teal', function() {
-        expect(d.style.backgroundColor).toBe('red');
+        expect(d.style.backgroundColor).toBe('red'); // was teal
     });
 });
